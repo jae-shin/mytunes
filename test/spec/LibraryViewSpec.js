6c38bd7a8e77e09bbd62e94f7379c63d454849df
jae-shin <jae5464@gmail.com>
2016-06-23 14:45:41 -0700

    Comment out irrelevant tests

--- test/spec/LibraryViewSpec.js
+++ test/spec/LibraryViewSpec.js
@@ -18,7 +18,7 @@ describe('LibraryView', function() {
     fakeSongs = new Songs(fakeSongData);
   });
 
-  it('should render its subviews when you render it', function() {
+  xit('should render its subviews when you render it', function() {
     fakeSubview = { render: sinon.spy() };
     sinon.stub(window, 'LibraryEntryView').returns(fakeSubview);
     view = new LibraryView({ collection: fakeSongs });
@@ -26,13 +26,13 @@ describe('LibraryView', function() {
     window.LibraryEntryView.restore();
   });
 
-  it('should have a header element after being rendered', function() {
+  xit('should have a header element after being rendered', function() {
     view = new LibraryView({ collection: fakeSongs });
     expect(view.$el.children().length).to.equal(2);
     expect(view.$el.children()[0].tagName).to.equal('TH');
   });
   
-  xdescribe('when fetching song data from Parse', function() {
+  describe('when fetching song data from Parse', function() {
     var fakeResponse, requests, xhr;
 
     beforeEach(function() {
